48e9e2f978f35b3ea3c7ee940fb0953809007459
monaka0710 <y.h.130710@outlook.jp>
2025-04-30 13:23:36 +0900

    課題の再提出

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,5 +1,7 @@
+// 変数の宣言・値の代入
 let weight = 68;
-
 let height = 1.7;
-
-console.log(68 /( 1.7 * 1.7));
+// 変数の宣言・値の代入・四則演算
+let result = (weight /( height * height));
+// コンソールへの出力
+console.log(result);
